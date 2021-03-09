84210a534e9f19ccdf9fef05dc0b5a2b43be8e4e
hsvsurya <hsvsurya@gmail.com>
2021-03-09 18:27:46 +0530

    chat_fix

--- lib/screens/account_screen.dart
+++ lib/screens/account_screen.dart
@@ -9,8 +9,45 @@ class _AccountScreenState extends State<AccountScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Account Screen'),
+      body: ListView(
+        padding: EdgeInsets.only(top: 70),
+        children: [
+          Row(
+            children: [
+              Container(
+                padding: EdgeInsets.only(left: 20),
+                child: Center(
+                  child: CircleAvatar(
+                    radius: 50,
+                    child: Icon(
+                      Icons.person,
+                      size: 70,
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.only(left: 20),
+                child: Text(
+                  'Surya',
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 20),
+          Divider(thickness: 1.2),
+          ListTile(
+            title: Text('Account'),
+            leading: Icon(Icons.person),
+            trailing: Icon(Icons.arrow_forward_ios),
+          ),
+          ListTile(
+            title: Text('Account'),
+            leading: Icon(Icons.person),
+            trailing: Icon(Icons.arrow_forward_ios),
+          ),
+        ],
       ),
     );
   }
